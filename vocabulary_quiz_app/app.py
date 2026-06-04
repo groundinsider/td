c17856f192f4b8107dcf964b4214737f9ddd48e2
groundinsider <groundinsider24@gmail.com>
2026-06-04 16:04:14 +0900

    setData

--- vocabulary_quiz_app/app.py
+++ vocabulary_quiz_app/app.py
@@ -2,7 +2,7 @@ import random
 import tkinter as tk
 from tkinter import ttk, font
 
-from vocabulary_quiz_app.quiz_logic import draw_word, check_answer, Word
+from vocabulary_quiz_app.quiz_logic import Word, check_answer, draw_word
 
 
 class VocabularyQuizApp:
